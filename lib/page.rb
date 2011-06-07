a15786911384621720ccb1ba78742c7b6815a2e0
lizconlan <Liz@liz-conlans-macbook.local>
2011-06-07 13:08:13 +0100

    making Page a bit more useful

--- lib/page.rb
+++ lib/page.rb
@@ -2,18 +2,19 @@ require 'nokogiri'
 require 'rest-client'
 
 class Page
-  attr_reader :html, :doc, :next_page
+  attr_reader :html, :doc, :url, :next_url
   
   def initialize(url)
+    @url = url
     response = RestClient.get(url)
     @html = response.body
     @doc = Nokogiri::HTML(@html)
     next_link = @doc.xpath("//div[@class='navLinks'][2]/div[@class='navLeft']/a")
     unless next_link.empty?
       prefix = url[0..url.rindex("/")]
-      @next_page = prefix + next_link.attr("href").value.to_s
+      @next_url = prefix + next_link.attr("href").value.to_s
     else
-      @next_page = nil
+      @next_url = nil
     end
   end
 end
