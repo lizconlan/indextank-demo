a3806f7c7070d0515abfbb51c04db8e9eeb84d96
lizconlan <Liz@liz-conlans-macbook.local>
2011-06-08 14:06:40 +0100

    making Page extract the metadata

--- lib/page.rb
+++ lib/page.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'rest-client'
 
 class Page
-  attr_reader :html, :doc, :url, :next_url
+  attr_reader :html, :doc, :url, :next_url, :start_column, :end_column, :volume, :part
   
   def initialize(url)
     @url = url
@@ -16,5 +16,18 @@ class Page
     else
       @next_url = nil
     end
+    scrape_metadata()
   end
+  
+  private
+    def scrape_metadata
+      subject = doc.xpath("//meta[@name='Subject']").attr("content").value.to_s
+      column_range = doc.xpath("//meta[@name='Columns']").attr("content").value.to_s
+      cols = column_range.gsub("Columns: ", "").split(" to ")
+
+      @start_column = cols[0]
+      @end_column = cols[1]
+      @volume = subject[subject.index("Volume:")+8..subject.rindex(",")-1]
+      @part = subject[subject.index("Part:")+6..subject.length]
+    end
 end
