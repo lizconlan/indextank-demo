54592ef519e1a7a429fc0c72aa52a74af6e7e038
lizconlan <Liz@liz-conlans-macbook.local>
2011-06-08 14:05:23 +0100

    making Parser generate a doc_id and accept an optional house param

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,16 +1,18 @@
 require 'nokogiri'
 require 'rest-client'
+require 'date'
 
 class Parser
-  attr_reader :date
+  attr_reader :date, :doc_id
   
-  def initialize(date)
+  def initialize(date, house="Commons")
     @date = date
+    @doc_id = "#{date}_hansard_#{house[0..0].downcase()}"
   end
   
   def get_section_links
     parse_date = Date.parse(date)
-    index_page = "http://www.parliament.uk/business/publications/hansard/commons/by-date/?d=#{parse_date.day}&m=#{parse_date.month}&y=#{parse_date.year}"
+    index_page = "http://www.parliament.uk/business/publications/hansard/#{house.downcase()}/by-date/?d=#{parse_date.day}&m=#{parse_date.month}&y=#{parse_date.year}"
     begin
       result = RestClient.get(index_page)
     rescue
