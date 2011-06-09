a85cfa783672e1f5c02e9abe1d0003ae0feacae5
lizconlan <Liz@liz-conlans-macbook.local>
2011-06-09 13:51:15 +0100

    basic html escaping; better url formatting

--- server.rb
+++ server.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'lib/test'
 
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+  def format_url(url)
+    url = url[url.index("//")+2..url.length]
+    if url.rindex("#") and url.rindex("/") and url.rindex("#") > url.rindex("/")
+      url = url[0..url.rindex("#")-1]
+    end
+    if url.length > 50
+      parts = url.split("/")
+      url = "<b>#{parts[0]}</b>/#{parts[1]}/#{parts[2]}/.../#{parts.last}"
+    end
+    url
+  end
+end
+
 get "/" do
   query = params[:s]
   if query and query.strip != ""
