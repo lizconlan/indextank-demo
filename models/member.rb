929ea8dd5281f0ea0cb3b86574b1e73ac38d4c91
lizconlan <lizconlan@gmail.com>
2011-10-10 12:56:44 +0100

    added Member.find_or_create method

--- models/member.rb
+++ models/member.rb
@@ -5,6 +5,28 @@ class Member
   key :post, String
   
   many :contributions
+  
+  
+  def self.find_or_create(name)
+    member = nil
+    name = name.strip
+    if name.downcase() =~ /^the / or name.downcase() =~ / speaker$/
+      member = Member.first(:post => name)
+      unless member
+        member = Member.new({:post => name})
+        member.save
+      end
+    else
+      if name.split(" ").length > 1
+        member = Member.first(:name => name)
+        unless member
+          member = Member.new({:name => name})
+          member.save
+        end
+      end
+    end
+    member
+  end
 end
 
 class Contribution
